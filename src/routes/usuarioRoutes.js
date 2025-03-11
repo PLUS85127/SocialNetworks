9f3b259fcdd4339f398d3d0b059f29c1c2fc7a08
PLUS85127 <caleph.ruiz@ulv.edu.mx>
2025-03-10 22:25:18 -0600

    Update usuarioRoutes.js

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const usuarioController = require('../controllers/usuarioController');
 const router = express.Router();
 
+//insertar mi usuario (para que pueda crear usuarios y asi hacer las pruebas)
 router.post('/', usuarioController.insertarUsuario);
+
+//obtener un usuario en especifico
 router.get('/:id', usuarioController.getUserById);
 
 module.exports = router;
