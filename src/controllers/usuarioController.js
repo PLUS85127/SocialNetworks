e603b60f92b71ed72a0d5ea61bb113fc675f1eb4
PLUS85127 <caleph.ruiz@ulv.edu.mx>
2025-03-10 22:08:50 -0600

    Update usuarioController.js

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,14 +1,17 @@
 const Usuario = require('../models/Usuario');
 
+//insertar usuario
 exports.insertarUsuario = async (req, res) => {
-  const { nombre, email } = req.body;
+  const { nombre, email } = req.body; //solo el nombre y el email (que es lo que tengo) en el body
 
   try {
+    //checar si el usuario se repite, me refiero a que no puedo repetr tanto nombre y email
     const usuarioExiste = await Usuario.findOne({ email });
     if (usuarioExiste) {
       return res.status(400).json({ success: false, message: 'El usuario ya existe' });
     }
 
+    //creo el usuario
     const nuevoUsuario = new Usuario({ nombre, email });
     await nuevoUsuario.save();
     res.status(201).json({ success: true, message: 'Usuario creado exitosamente', data: nuevoUsuario });
@@ -17,9 +20,11 @@ exports.insertarUsuario = async (req, res) => {
   }
 };
 
+//traer el usuario con el id
 exports.getUserById = async (req, res) => {
   try {
-    const usuario = await Usuario.findById(req.params.id);
+    const usuario = await Usuario.findById(req.params.id);//busoc el usuario por el id
+    //si no lo encuentro que me retorne el mensaje que no se encontro
     if (!usuario) {
       return res.status(404).json({ success: false, message: 'Usuario no encontrado' });
     }
