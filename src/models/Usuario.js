12d0a7feb7b6389b01959e07c55c6535651e2280
PLUS85127 <caleph.ruiz@ulv.edu.mx>
2025-03-10 22:20:06 -0600

    Update Usuario.js

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//mi esquema de usuario 
 const usuarioSchema = new Schema({
+  //nombre de mi usuario pero esta vez le indico que sea unico
   nombre: { type: String, required: true, unique: true },
+  //al igual que email que sea unico
   email: { type: String, required: true, unique: true }
 }, { timestamps: true });
 
