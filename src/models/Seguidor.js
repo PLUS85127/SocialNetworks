7502146267e930c44c4aae2eaf24d101eca8cfee
PLUS85127 <caleph.ruiz@ulv.edu.mx>
2025-03-10 22:17:51 -0600

    Update Seguidor.js

--- src/models/Seguidor.js
+++ src/models/Seguidor.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//mi esquema para seguidor
 const seguidorSchema = new Schema({
+  //id del usuario que va a seguir (osea el que sigue) usando Usuario
   usuarioId: { type: Schema.Types.ObjectId, ref: 'Usuario', required: true },
+  //id del usuario seguido (el queen estoy siguiendo)
   seguidoId: { type: Schema.Types.ObjectId, ref: 'Usuario', required: true }
 }, { timestamps: true });
 
