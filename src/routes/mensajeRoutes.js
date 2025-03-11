9fb13999d26b4c5a8b3684afbf158bb4124bad30
PLUS85127 <caleph.ruiz@ulv.edu.mx>
2025-03-10 22:22:02 -0600

    Update mensajeRoutes.js

--- src/routes/mensajeRoutes.js
+++ src/routes/mensajeRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const mensajeController = require('../controllers/mensajeController');
+const mensajeController = require('../controllers/mensajeController');//mi ruta de mensaje
 const router = express.Router();
 
+//mis rutas que voy a utilizar para mis mensajes
+
+//enviar
 router.post('/', mensajeController.enviarMensaje);
+
+//obtener (mensajes recibidos)
 router.get('/recibidos', mensajeController.mensajesRecibidos);
 
 module.exports = router;
